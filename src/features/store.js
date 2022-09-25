0351de43f39d93819686460d7829b38f3565c16e
Chris Joe <flamer.ohr@gmail.com>
2022-09-26 12:30:53 +1300

    hook up api slice to the store

--- src/features/store.js
+++ src/features/store.js
@@ -1,9 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { Provider } from "react-redux";
+import { apiSlice } from "./apiSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
+
+// optional, could remove
+setupListeners(store.dispatch);
 
 export const withStore = (Component) => {
   const StoreProvider = () => {
