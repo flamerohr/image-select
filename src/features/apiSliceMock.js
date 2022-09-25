7b93253c8230de5f712f10bd98846451690b3e76
Chris Joe <flamer.ohr@gmail.com>
2022-09-26 12:39:05 +1300

    add api mock for testing

---
+++ src/features/apiSliceMock.js
@@ -0,0 +1,81 @@
+export const useImagesQuery = () => ({
+  isLoading: false,
+  isError: false,
+  isSuccess: true,
+  error: null,
+  data: [
+    {
+      id: "261e8d344e894aea83b94acd8b0bca33",
+      filename: "courtney-and-jackson-652.jpg",
+      url: "https://select-test-images.s3.amazonaws.com/courtney-and-jackson-652.jpg",
+    },
+    {
+      id: "38313e3b81774f009d8e32b337463cc0",
+      filename: "courtney-and-jackson-645.jpg",
+      url: "https://select-test-images.s3.amazonaws.com/courtney-and-jackson-645.jpg",
+    },
+    {
+      id: "425297c245244b63b9fb3399ebe7f1f8",
+      filename: "courtney-and-jackson-642.jpg",
+      url: "https://select-test-images.s3.amazonaws.com/courtney-and-jackson-642.jpg",
+    },
+    {
+      id: "57499552dc614d998ad517ce103d0a41",
+      filename: "courtney-and-jackson-654.jpg",
+      url: "https://select-test-images.s3.amazonaws.com/courtney-and-jackson-654.jpg",
+    },
+    {
+      id: "73a3acd0f4ca45ac9719112e55cddcd1",
+      filename: "courtney-and-jackson-650.jpg",
+      url: "https://select-test-images.s3.amazonaws.com/courtney-and-jackson-650.jpg",
+    },
+    {
+      id: "7ed7e558db134412b8089e2d6bf1a3a8",
+      filename: "courtney-and-jackson-638.jpg",
+      url: "https://select-test-images.s3.amazonaws.com/courtney-and-jackson-638.jpg",
+    },
+    {
+      id: "aa4f374a08c54a0aab5f5447ec1668e4",
+      filename: "courtney-and-jackson-632.jpg",
+      url: "https://select-test-images.s3.amazonaws.com/courtney-and-jackson-632.jpg",
+    },
+    {
+      id: "bc5d7f78cdbf4855b385c3907a97aa6e",
+      filename: "courtney-and-jackson-659.jpg",
+      url: "https://select-test-images.s3.amazonaws.com/courtney-and-jackson-659.jpg",
+    },
+    {
+      id: "c155f291875c479b89f05c5839d0b14b",
+      filename: "courtney-and-jackson-656.jpg",
+      url: "https://select-test-images.s3.amazonaws.com/courtney-and-jackson-656.jpg",
+    },
+    {
+      id: "c8856f4812404de1adfc9df63bdc76fc",
+      filename: "courtney-and-jackson-637.jpg",
+      url: "https://select-test-images.s3.amazonaws.com/courtney-and-jackson-637.jpg",
+    },
+  ],
+});
+
+export const useImageFacesQuery = () => ({
+  isLoading: false,
+  isError: false,
+  isSuccess: true,
+  error: null,
+  data: [
+    {
+      id: "392614b3e2f146c9a0c9d643d8b8450f",
+      xmin: 0.2488081455230713,
+      ymin: 0.28436192870140076,
+      ymax: 0.5191940665245056,
+      xmax: 0.35970163345336914,
+    },
+    {
+      id: "ce562aea054742879b42ba8994540314",
+      ymin: 0.25028282403945923,
+      ymax: 0.4674367308616638,
+      xmin: 0.5474767684936523,
+      xmax: 0.6666020154953003,
+    },
+  ],
+});
